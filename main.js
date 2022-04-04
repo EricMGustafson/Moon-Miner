1800dc4d856d45b0c0cf3cea2fa7e36e5d616c89
EricMGustafson <emgustafson22@gmail.com>
2022-04-04 00:22:18 -0600

    final commit

--- main.js
+++ main.js
@@ -3,34 +3,34 @@
 let rupees = 0
 let stealTotal = 0
 let passiveTotal = 0
-let savedGame = []
 let bonusTotal = 0
 let allTotal = 0
 let clickCount = 0
+let savedGame = []
 
 const stealUpgrades = {
   stickyFingers: {
     name: 'Sticky Fingers',
-    price: 0,
+    price: 20,
     quantity: 0,
-    bonus: 1,
+    bonus: 0,
     multiplier: 2,
     img: './assets/thief.jpg'
   },
   smashPots: {
     name: 'Smash Pots',
-    price: 2500,
+    price: 100,
     quantity: 0,
-    bonus: 1,
-    multiplier: 6,
+    bonus: 0,
+    multiplier: 3,
     img: './assets/lots-o-pots.jpg'
   },
   masterSword: {
     name: 'Master Sword',
-    price: 5000,
+    price: 500,
     quantity: 0,
-    bonus: 1,
-    multiplier: 10,
+    bonus: 0,
+    multiplier: 4,
     img: './assets/master-sword.png'
   }
 };
@@ -38,26 +38,26 @@ const stealUpgrades = {
 const passiveUpgrades = {
   rolling: {
     name: 'Rolling',
-    price: 50,
+    price: 60,
     quantity: 0,
-    bonus: 1,
-    multiplier: 3,
+    bonus: 0,
+    multiplier: 5,
     img: './assets/rolling.png'
   },
   spinAttack: {
     name: 'Spin Attack',
-    price: 10000,
+    price: 300,
     quantity: 0,
-    bonus: 1,
-    multiplier: 7,
+    bonus: 0,
+    multiplier: 6,
     img: './assets/spin-attack.gif'
   },
   bomb: {
     name: 'Bomb',
-    price:  20000,
+    price:  1500,
     quantity: 0,
-    bonus: 1,
-    multiplier: 11,
+    bonus: 0,
+    multiplier: 7,
     img: './assets/bomb.webp'
   }
 };
@@ -131,10 +131,11 @@ Swal.fire({
 }
 
 function bigToast(title) {
+  // @ts-ignore
   Swal.fire({
     title: title,
     text: 'You have collect 1,000,000 rupees!!',
-    imageUrl: 'https://unsplash.it/400/200',
+    imageUrl: 'https://unsphttps://i.makeagif.com/media/6-09-2015/ytZUe1.giflash.it/400/200',
     imageWidth: 400,
     imageHeight: 200,
     imageAlt: 'Make it Rain',
@@ -146,14 +147,10 @@ function drawStealCards() {
   for (let upgrades in stealUpgrades) {
       let upgrade = stealUpgrades[upgrades]
       template += `
-      <div id="${upgrade.name}" style="background-image: url('${upgrade.img}')" class="col-4 w-100 upgrade-tile select bg-image m-3 justify-content-between fs-4" onclick="buyStealUpgrade('${upgrades}')">
-        <div class="p-2 h-100">
-          <p class="m-2"><span class="upgrade-bg">${upgrade.name}:</span></p>
-          <p class="m-2"><span class="upgrade-bg">Cost:</span</p>
-        </div>
-        <div class="p-2 h-100">
-          <p class="m-2"><span class="upgrade-bg">${upgrade.quantity}</span></p>
-          <p class="m-2"><span class="upgrade-bg">${upgrade.price}</span</p>
+      <div id="${upgrade.name}" style="background-image: url('${upgrade.img}')" class="col upgrade-tile select bg-image" onclick="buyStealUpgrade('${upgrades}')">
+        <div class="d-flex flex-column">
+          <p><span class="upgrade-bg">${upgrade.name}:</span><span class="upgrade-bg">${upgrade.quantity}</span></p>
+          <p><span class="upgrade-bg">Cost: </span><span class="upgrade-bg">${upgrade.price}</span</p>
         </div>
       </div>`
     }
@@ -165,14 +162,10 @@ function drawPassiveCards() {
   for (let upgrades in passiveUpgrades) {
       let upgrade = passiveUpgrades[upgrades];
       template += `
-      <div id="${upgrade.name}" style="background-image: url('${upgrade.img}')" class="col-4 w-100 upgrade-tile select bg-image m-3 justify-content-between fs-4" onclick="buyPassiveUpgrade('${upgrades}')">
-        <div class="p-2 h-100">
-        <p class="m-2"><span class="upgrade-bg">${upgrade.name}:</span></p>
-        <p class="m-2"><span class="upgrade-bg">Cost:</p>
-        </div>
-        <div class="p-2 h-100">
-          <p class="m-2"><span class="upgrade-bg">${upgrade.quantity}</span></p>
-          <p class="m-2"><span class="upgrade-bg">${upgrade.price}</span</p>
+      <div id="${upgrade.name}" style="background-image: url('${upgrade.img}')" class="col-3 upgrade-tile select bg-image py-1" onclick="buyPassiveUpgrade('${upgrades}')">
+        <div class="d-flex flex-column">
+          <p><span class="upgrade-bg">${upgrade.name}:</span><span class="upgrade-bg">${upgrade.quantity}</span></p>
+          <p><span class="upgrade-bg">Cost: </span><span class="upgrade-bg">${upgrade.price}</span</p>
         </div>
       </div>`
   }
@@ -203,34 +196,34 @@ function drawBadge() {
       const badge = allBadges[badges];
       template +=`
         <div>
-          <img class="object-fit p-3" src=${badge.img} alt="" title="${badge.title}">
+          <img class="object-fit px-2" src=${badge.img} alt="" title="${badge.title}">
         </div>`
     }
     document.getElementById('badges').innerHTML = template
 }
 
 function drawBadgeWin(){
-  if (stealUpgrades.stickyFingers.quantity == 5){
+  if (stealUpgrades.stickyFingers.quantity == 5 && allBadges.blue.img != allBadges.blue.badgeimg){
     allBadges.blue.img = allBadges.blue.badgeimg
     toast('Blue Badge', 'You have purchased 5 Sticky Fingers upgrades!')
   }
-  if (passiveUpgrades.rolling.quantity == 5) {
+  if (passiveUpgrades.rolling.quantity == 5 && allBadges.green.img != allBadges.green.badgeimg) {
     allBadges.green.img = allBadges.green.badgeimg
     toast('Green Badge', 'You have purchased 5 Rolling upgrades!')
   }
-  if (clickCount == 500){
+  if (clickCount >= 500 && allBadges.orange.img != allBadges.orange.badgeimg){
     allBadges.orange.img = allBadges.orange.badgeimg
     toast('Orange Badge', 'You have stolen from the pot 500 times!')
   }
-  if (stealUpgrades.masterSword.quantity == 1) {
+  if (stealUpgrades.masterSword.quantity == 1 && allBadges.purple.img != allBadges.purple.badgeimg) {
     allBadges.purple.img = allBadges.purple.badgeimg
     toast('Purple Badge', 'You have purchased the Master Sword upgrade!')
   }
-  if (passiveUpgrades.bomb.quantity == 1) {
+  if (passiveUpgrades.bomb.quantity == 1 && allBadges.red.img != allBadges.red.badgeimg) {
     allBadges.red.img = allBadges.red.badgeimg
     toast('Red Badge', 'You have purchased the Bomb upgrade!')
   }
-  if (count == 1000000) {
+  if (rupees >= 1000000 && allBadges.silver.img != allBadges.silver.badgeimg) {
     allBadges.silver.img = allBadges.silver.badgeimg
     bigToast('Silver Badge')
   }
@@ -283,8 +276,9 @@ function buyStealUpgrade(upgrade) {
   if (rupees >= stealUpgrades[upgrade].price) {
     rupees -= stealUpgrades[upgrade].price
     stealUpgrades[upgrade].quantity++
-    stealUpgrades[upgrade].bonus += (stealUpgrades[upgrade].multiplier * stealUpgrades[upgrade].bonus)
-    stealUpgrades[upgrade].price += (stealUpgrades[upgrade].multiplier * 100)
+    stealUpgrades[upgrade].bonus++
+    stealUpgrades[upgrade].bonus *= stealUpgrades[upgrade].multiplier
+    stealUpgrades[upgrade].price += (stealUpgrades[upgrade].multiplier * 20)
     addAllSteals(upgrade)
     update()
     drawStealCards()
@@ -293,7 +287,7 @@ function buyStealUpgrade(upgrade) {
     drawStealBonus()
     drawBadgeWin()
   } else {
-      window.alert('Not enough Rupees to purchase the ' + stealUpgrades[upgrade].name + ' upgrade.')
+    toast('Bummer','You do not have enough for this purchase.')
     }
 }
 
@@ -317,29 +311,6 @@ function update() {
   document.getElementById('all-total').innerText = JSON.stringify(allTotal)
 }
 
-// function buyStickyFingers() {
-//   let upgrade = stealUpgrades.stickyFingers
-//   if (rupees >= upgrade.price) {
-//     rupees -= upgrade.price
-//     upgrade.quantity += 1
-//     stealTotal += upgrade.bonus
-//   }
-//   addUpgrade(upgrade)
-//   update()
-// }
-
-// function buySmashPots() {
-//   let upgrade = stealUpgrades.smashPots
-//   if (rupees >= upgrade.price) {
-//     rupees -= upgrade.price
-//     upgrade.quantity += 1
-//     stealTotal += upgrade.bonus
-//   }
-//   addUpgrade(upgrade)
-//   update()
-// }
-
-
 // #endregion
 
 // #region passives
@@ -348,16 +319,18 @@ function buyPassiveUpgrade(upgrade) {
   if (rupees >= passiveUpgrades[upgrade].price) {
     rupees -= passiveUpgrades[upgrade].price
     passiveUpgrades[upgrade].quantity++
-    passiveUpgrades[upgrade].bonus += (passiveUpgrades[upgrade].multiplier * passiveUpgrades[upgrade].bonus)
-    passiveUpgrades[upgrade].price += (passiveUpgrades[upgrade].multiplier * 100)
+    passiveUpgrades[upgrade].bonus++
+    passiveUpgrades[upgrade].bonus *= passiveUpgrades[upgrade].multiplier
+    passiveUpgrades[upgrade].price += (passiveUpgrades[upgrade].multiplier * 20)
   } else {
-    window.alert('Not enough Rupees to purchase the ' + passiveUpgrades[upgrade].name + ' upgrade.')
+    toast('Bummer','You do not have enough for this purchase.')
   }
   addPassiveUpgrade(upgrade)
   update()
   drawPassiveCards()
   hidePassiveUpgrades()
   drawPassiveBonus()
+  drawBadgeWin()
 }
 
 
@@ -370,42 +343,8 @@ function addPassiveUpgrade(total) {
 
 function totalPassiveUpgrades(){
   rupees += passiveTotal
+  update()
 }
-
-// function buyRolling() {
-//   let upgrade = passiveUpgrades.rolling
-//   if (rupees >= upgrade.price) {
-//     upgrade.quantity += 1
-//     upgrade.bonus += passiveTotal
-//   }
-//   addPassiveUpgrade()
-//   update()
-//   drawPassiveCards()
-// }
-// function buySpinAttack() {
-//   let upgrade = passiveUpgrades.spinAttack
-//   if (rupees >= upgrade.price) {
-//     upgrade.quantity += 1
-//     upgrade.bonus += passiveTotal
-//   }
-//   addPassiveUpgrade()
-//   update()
-//   drawPassiveCards()
-// }
-// function buyBomb() {
-//   let upgrade = passiveUpgrades.bomb
-//   if (rupees >= upgrade.price) {
-//     rupees -= upgrade.price
-//     upgrade.quantity += 1
-//     upgrade.bonus += 1
-//     upgrade.price += (upgrade.multiplier * 300)
-//   }
-//   addPassiveUpgrade()
-//   update()
-//   drawPassiveCards()
-// }
-// 
-
 
 // #endregion 
 
